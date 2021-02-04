bafd49485a5b87998452cbca7e6dc32857206252
Frityet <ambhogal01@gmail.com>
2021-02-03 22:27:24 -0800

    Honestly idk what im doing

---
+++ Assets/Scripts/TestUnityEvents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TestUnityEvents : MonoBehaviour
+{
+    public void TestMethod1(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public void TestMethod2(int[] numbers)
+    {
+        var nums = 0;
+        foreach (var num in numbers)
+        {
+            nums += num;
+        }
+
+        Debug.Log(nums);
+    }
+}
